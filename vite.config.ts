d986d714a4d31de2aa0de70d756ebd950bc55428
writeKhoa <khoawrite@gmail.com>
2023-02-06 18:13:14 +0700

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
